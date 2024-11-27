abf6d6e2f473953fb3c50298c7e24ba99337c70c
Davilapeixoto <davilapeixoto52@gmail.com>
2024-11-27 18:14:57 -0300

    ultimos updates

--- codigo.js
+++ codigo.js
@@ -1,19 +1,15 @@
-document.getElementById('mapa').addEventListener('click', function () {
-    // Exibe o mapa ao clicar no botão
-    const mapContainer = document.getElementById('map');
-    mapContainer.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('mapa').addEventListener('click', function () {
+        const mapContainer = document.getElementById('map');
+        mapContainer.style.display = 'block';
+        const map = L.map('map').setView([-8.58333, -35.38333], 13); 
 
-    // Inicializa o mapa em Gameleira
-    const map = L.map('map').setView([-8.58333, -35.38333], 13); // Latitude, longitude e zoom
-
-    // Adiciona a camada de mapa do OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-
-    // Adiciona um marcador em Gameleira
-    L.marker([-8.58333, -35.38333]).addTo(map)
-        .bindPopup('Gameleira, PE.<br>Bem-vindo!')
-        .openPopup();
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+        L.marker([-8.58333, -35.38333]).addTo(map)
+            .bindPopup('Gameleira, PE.<br>')
+            .openPopup();
+    });
 });
